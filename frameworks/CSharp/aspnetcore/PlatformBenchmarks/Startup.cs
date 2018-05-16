03ae42ef61e4ed4a035817cec41b039105107407
Sebastien Ros <sebastienros@gmail.com>
2018-05-16 14:34:44 -0700

    Adding missing MySql references

--- frameworks/CSharp/aspnetcore/PlatformBenchmarks/Startup.cs
+++ frameworks/CSharp/aspnetcore/PlatformBenchmarks/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Npgsql;
+using MySql.Data.MySqlClient;
 
 namespace PlatformBenchmarks
 {
