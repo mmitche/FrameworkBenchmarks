804d17c47c5df728d72018783657796b490469b3
Sebastien Ros <sebastienros@gmail.com>
2019-01-31 18:04:56 -0800

    Using a single endpoint

--- frameworks/JavaScript/nodejs/create-server.js
+++ frameworks/JavaScript/nodejs/create-server.js
@@ -12,34 +12,31 @@ const basicHandler = routing.BasicHandler;
 const queryHandler = routing.QueryHandler;
 const routeNotImplemented = require('./helper').responses.routeNotImplemented;
 
-var app = function (req, res) {
-  const url = parseurl(req);
-  const route = url.pathname;
-
-  // Routes that do no require a `queries` parameter
-  if (basicHandler.has(route)) {
-    return basicHandler.handle(route, req, res);
-  } else {
-    // naive: only works if there is one query param, as is the case in TFB
-    let queries = url.query && url.query.split('=')[1];
-    queries = ~~(queries) || 1;
-    queries = Math.min(Math.max(queries, 1), 500);
-
-    if (queryHandler.has(route)) {
-      return queryHandler.handle(route, queries, req, res);
-    } else {
-      return routeNotImplemented(req, res);
-    }
-  }
-};
-
 module.exports = function() {
-  http.createServer(app).listen(8080, () => console.log("NodeJS worker listening on port 8080"));
-
   const options = {
     pfx: fs.readFileSync('testCert.pfx'),
     passphrase: 'testPassword'
   };
 
-  https.createServer(options, app).listen(8081, () => console.log("NodeJS worker listening on port 8081"));
+  https.createServer(options, function (req, res) {
+    const url = parseurl(req);
+    const route = url.pathname;
+
+    // Routes that do no require a `queries` parameter
+    if (basicHandler.has(route)) {
+      return basicHandler.handle(route, req, res);
+    } else {
+      // naive: only works if there is one query param, as is the case in TFB
+      let queries = url.query && url.query.split('=')[1];
+      queries = ~~(queries) || 1;
+      queries = Math.min(Math.max(queries, 1), 500);
+
+      if (queryHandler.has(route)) {
+        return queryHandler.handle(route, queries, req, res);
+      } else {
+        return routeNotImplemented(req, res);
+      }
+    }
+
+  }).listen(8080, () => console.log("NodeJS worker listening on port 8080"));
 }
